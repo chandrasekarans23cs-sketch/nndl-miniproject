3f2f446a0c1e58477f75a118ca7b41bf62f35165
chandrasekarans23cs-sketch <chandrasekarans23cs@psnacet.edu.in>
2025-12-12 05:49:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,25 +8,18 @@ from utils import preprocess_image, postprocess_output
 model = load_model("colorization_model.h5")
 
 st.title("🎨 Black & White Image Colorization")
-st.write("Upload a grayscale photo and let the CNN bring it to life!")
+st.write("Upload a grayscale photo or choose a sample to see it colorized!")
 
-# File uploader
-uploaded_file = st.file_uploader("Upload a black & white image", type=["jpg", "png"])
+uploaded_file = st.file_uploader("Upload a grayscale image", type=["jpg", "png"])
 
 if uploaded_file is not None:
-    # Read image
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     gray_img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
 
     st.image(gray_img, caption="Original Grayscale", use_column_width=True)
 
-    # Preprocess
     input_img = preprocess_image(gray_img)
-
-    # Predict color channels
     ab_channels = model.predict(input_img)
-
-    # Postprocess to get final colorized image
     colorized_img = postprocess_output(gray_img, ab_channels)
 
     st.image(colorized_img, caption="Colorized Output", use_column_width=True)
